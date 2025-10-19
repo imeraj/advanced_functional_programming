4f322c7cc4b3babc50edfc36ab020cfbd5593591
Meraj Molla <meraj.enigma@gmail.com>
2025-10-19 06:50:28 -0400

    define equality

--- fun_park/lib/fun_park/monad/maybe/just.ex
+++ fun_park/lib/fun_park/monad/maybe/just.ex
@@ -12,3 +12,15 @@ defimpl FunPark.Foldable, for: FunPark.Monad.Maybe.Just do
   def fold_l(%Just{value: value}, just_fn, _nothing_fn), do: just_fn.(value)
   def fold_r(just, just_fn, nothing_fn), do: fold_l(just, just_fn, nothing_fn)
 end
+
+defimpl FunPark.Eq, for: FunPark.Monad.Maybe.Just do
+  alias FunPark.Eq
+  alias FunPark.Monad.Maybe.Just
+  alias FunPark.Monad.Maybe.Nothing
+
+  def eq?(%Just{value: v1}, %Just{value: v2}), do: Eq.eq?(v1, v2)
+  def eq?(%Just{}, %Nothing{}), do: false
+
+  def not_eq?(%Just{value: v1}, %Just{value: v2}), do: not Eq.eq?(v1, v2)
+  def not_eq?(%Just{}, %Nothing{}), do: true
+end
--- fun_park/lib/fun_park/monad/maybe/nothing.ex
+++ fun_park/lib/fun_park/monad/maybe/nothing.ex
@@ -10,3 +10,14 @@ defimpl FunPark.Foldable, for: FunPark.Monad.Maybe.Nothing do
   def fold_l(%Nothing{}, _just_fn, nothing_fn), do: nothing_fn.()
   def fold_r(nothing, just_fn, nothing_fn), do: fold_l(nothing, just_fn, nothing_fn)
 end
+
+defimpl FunPark.Eq, for: FunPark.Monad.Maybe.Nothing do
+  alias FunPark.Monad.Maybe.Just
+  alias FunPark.Monad.Maybe.Nothing
+
+  def eq?(%Nothing{}, %Nothing{}), do: true
+  def eq?(%Nothing{}, %Just{}), do: false
+
+  def not_eq?(%Nothing{}, %Nothing{}), do: false
+  def not_eq?(%Nothing{}, %Just{}), do: true
+end
